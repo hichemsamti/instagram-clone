687296af7d84de35b0e4e4a25ad8332aec77f4f7
hichemsamti <samtihichem@gmail.com>
2021-06-28 21:08:00 +0100

    css for profile and home components

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -2,6 +2,97 @@ import React from 'react'
 
 export default function Home() {
     return (
-      <h1>Home</h1>
+      <div className="home">
+
+        <div className="card home-card">
+
+             <h5>hichem</h5>
+
+             <div className="card-image">
+
+                <img src="https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+
+             </div>
+
+
+             <div className="card-content">
+
+             <i className="material-icons" style={{color:"red"}}>favorite</i>
+
+               <h6>title</h6>
+
+               <p>this is amazing post</p>
+
+               <input type="text" placeholder="add a comment"/>
+
+
+             </div>
+
+
+        </div>
+
+
+
+
+        <div className="card home-card">
+
+             <h5>hichem</h5>
+
+             <div className="card-image">
+
+                <img src="https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+
+             </div>
+
+
+             <div className="card-content">
+
+
+             <i className="material-icons" style={{color:"red"}}>favorite</i>
+
+               <h6>title</h6>
+
+               <p>this is amazing post</p>
+
+               <input type="text" placeholder="add a comment"/>
+
+
+             </div>
+
+
+        </div>
+        
+        
+        
+        
+        <div className="card home-card">
+
+             <h5>hichem</h5>
+
+             <div className="card-image">
+
+                <img src="https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+
+             </div>
+
+
+             <div className="card-content">
+
+               <i className="material-icons" style={{color:"red"}}>favorite</i>
+
+               <h6>title</h6>
+
+               <p>this is amazing post</p>
+
+               <input type="text" placeholder="add a comment"/>
+
+
+             </div>
+
+
+        </div>
+
+
+      </div>
     )
 }
--- client/src/components/screens/Profile.jsx
+++ client/src/components/screens/Profile.jsx
@@ -2,24 +2,51 @@ import React from 'react'
 
 export default function Profile() {
     return (
-        <div>
+        <div style={{maxWidth:"550px", margin:"0px auto"}}>
 
-            <div>
+            <div style={{
+                display:"flex",
+                justifyContent:'space-around',
+                margin:"18px 0px",
+                borderBottom:"1px solid grey"
+            }} >
 
 
-               <div>
+                  <div>
 
-                   <img style={{width:"160px",height:"160px",borderRadius:"80px"}}/>
+                   <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+                   src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt=""
+                   
+                   />
 
-               </div>
+                  </div>
 
-               <div></div>
+                  <div>
+                 
+                  <h4>Hichem SAMTI</h4>
+
+                  <div style={{display:"flex", justifyContent:"space-between", width:"110%"}}>
+                      <h6>40 posts</h6>
+                      <h6>40 followers</h6>
+                      <h6>40 following</h6>
+                  </div>
+
+            </div>
 
            
            
             </div>
 
+            <div className="gallery">
+                <img className="item" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt=""/>
 
+
+
+            </div>
 
 
 
